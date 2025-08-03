0073290622dca63908ea4c0a4682d4d0478cdd9c
Asaf Yosef <asafsaf3@gmail.com>
2025-08-03 11:45:42 +0300

    add db services

---
+++ src/app/core/services/providers/mockup.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { dbInt } from "./db-int";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Mockup implements dbInt {
+  
+}
---
+++ src/app/core/services/services-module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { LocalDb } from './local-db';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LocalDb
+  ]
+})
+export class ServicesModule { }
