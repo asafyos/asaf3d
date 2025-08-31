87d0e28b1f51775d4d07b2055c38dc1d14c6533c
Asaf Yosef <asafsaf3@gmail.com>
2025-08-31 12:54:07 +0300

    add colors to mock

--- src/app/core/local-db/mock/mock-data/colors.ts
+++ src/app/core/local-db/mock/mock-data/colors.ts
@@ -7,14 +7,14 @@ export const colors: Color[] = [
         imageUrl: "",
         kgPrice: 86,
         outOfStock: false,
-        colorTag: "#000000",
-        colorTag2: "#000000"
+        colorTag: "#262521",
+        colorTag2: "#262521"
     },
     {
         id: 2,
         name: "לבן",
         imageUrl: "",
-        kgPrice: 65,
+        kgPrice: 89,
         outOfStock: false,
         colorTag: "#FFFFFF",
         colorTag2: "#FFFFFF"
@@ -23,18 +23,126 @@ export const colors: Color[] = [
         id: 3,
         name: "אדום",
         imageUrl: "",
-        kgPrice: 85,
+        kgPrice: 89,
         outOfStock: false,
-        colorTag: "#FF0000",
-        colorTag2: "#FF0000"
+        colorTag: "#B0182A",
+        colorTag2: "#B0182A"
     },
     {
         id: 4,
+        name: "כחול",
+        imageUrl: "",
+        kgPrice: 89,
+        outOfStock: false,
+        colorTag: "#132A86",
+        colorTag2: "#132A86"
+    },
+    {
+        id: 5,
+        name: "צהוב",
+        imageUrl: "",
+        kgPrice: 89,
+        outOfStock: false,
+        colorTag: "#FEE128",
+        colorTag2: "#FEE128"
+    },
+    {
+        id: 6,
+        name: "ירוק",
+        imageUrl: "",
+        kgPrice: 89,
+        outOfStock: false,
+        colorTag: "#264D2C",
+        colorTag2: "#264D2C"
+    },
+    {
+        id: 7,
+        name: "כתום",
+        imageUrl: "",
+        kgPrice: 86,
+        outOfStock: false,
+        colorTag: "#F5953F",
+        colorTag2: "#F5953F"
+    },
+    {
+        id: 8,
+        name: "אפרסק",
+        imageUrl: "",
+        kgPrice: 65,
+        outOfStock: false,
+        colorTag: "#F35438",
+        colorTag2: "#F35438"
+    },
+    {
+        id: 10,
+        name: "ורוד",
+        imageUrl: "",
+        kgPrice: 65,
+        outOfStock: false,
+        colorTag: "#F769B2",
+        colorTag2: "#F769B2"
+    },
+    {
+        id: 11,
+        name: "סגול",
+        imageUrl: "",
+        kgPrice: 86,
+        outOfStock: false,
+        colorTag: "#5D345A",
+        colorTag2: "#5D345A"
+    },
+    {
+        id: 12,
+        name: "אפור",
+        imageUrl: "",
+        kgPrice: 89,
+        outOfStock: false,
+        colorTag: "#D1BBB8",
+        colorTag2: "#D1BBB8"
+    },
+    {
+        id: 13,
+        name: "מנטול",
+        imageUrl: "",
+        kgPrice: 65,
+        outOfStock: false,
+        colorTag: "#B8A395",
+        colorTag2: "#B8A395"
+    },
+    {
+        id: 14,
+        name: "תורכיז",
+        imageUrl: "",
+        kgPrice: 89,
+        outOfStock: false,
+        colorTag: "#A7E0C8",
+        colorTag2: "#A7E0C8"
+    },
+    {
+        id: 15,
+        name: "חום",
+        imageUrl: "",
+        kgPrice: 65,
+        outOfStock: false,
+        colorTag: "#682C24",
+        colorTag2: "#682C24"
+    },
+    {
+        id: 16,
         name: "סגול-זהב",
         imageUrl: "",
         kgPrice: 115,
         outOfStock: false,
-        colorTag: "#4E007A",
-        colorTag2: "#E4A700"
+        colorTag: "#5D345A",
+        colorTag2: "#B38302"
+    },
+    {
+        id: 17,
+        name: "תורכיז-כחול",
+        imageUrl: "",
+        kgPrice: 115,
+        outOfStock: false,
+        colorTag: "#007A51",
+        colorTag2: "#00E4C6"
     },
 ]
