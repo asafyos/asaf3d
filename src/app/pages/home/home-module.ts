e79e868ecfe9091c9256fa94a406f38bbd85b5be
Asaf Yosef <asafsaf3@gmail.com>
2025-09-02 20:06:43 +0300

    move model-card to globals

--- src/app/pages/home/home-module.ts
+++ src/app/pages/home/home-module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { ModelCard } from './components/model-card/model-card';
+import { ModelCard } from '../../components/model-card/model-card';
 import { HomeRoutingModule } from './home-routing-module';
 import { HomePage } from './home.page';
+
 
 
 @NgModule({
   declarations: [
     HomePage,
-    ModelCard
   ],
   imports: [
     CommonModule,
@@ -21,6 +21,7 @@ import { HomePage } from './home.page';
     MatButtonModule,
     MatIconModule,
     AsyncPipe,
+    ModelCard,
   ]
 })
 export class HomeModule { }
